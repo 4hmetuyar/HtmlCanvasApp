6e58534a5704849174ada228b715e6c3d4b0dfc6
Ahmet Uyar <4hmetuyar@gmail.com>
2019-11-23 11:56:10 +0300

    İnitial Commit

---
+++ HtmlCanvasApp/HtmlCanvasApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HtmlCanvasApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HtmlCanvasApp/HtmlCanvasApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HtmlCanvasApp.MvcApplication" Language="C#" %>

451afa1828cfa9a3b69a4d6418e6ce8cfa48430f
Ahmet Uyar <4hmetuyar@gmail.com>
2019-11-23 23:13:38 +0300

    Html Canvas.

--- HtmlCanvasApp/HtmlCanvasApp/Controllers/HomeController.cs
+++ HtmlCanvasApp/HtmlCanvasApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Web.Mvc;
 
 namespace HtmlCanvasApp.Controllers
 {
@@ -7,6 +10,34 @@ namespace HtmlCanvasApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveImage(string imageData)
+        {
+            Random rnd = new Random();
+            String filename = "image_" + rnd.Next(12, 2000).ToString() + ".png";
+            string picPath = System.Web.HttpContext.Current.Server.MapPath(filename);
+            using (FileStream fs = new FileStream(picPath, FileMode.Create))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    byte[] data = Convert.FromBase64String(imageData);
+                    bw.Write(data);
+                    bw.Close();
+                }
+            }
+
+            return Json(true);
+        }
+
+        private static void SaveTxtFile(string img)
+        {
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(System.Web.HttpContext.Current.Server.MapPath("~/file.txt")))
+            {
+                file.WriteLine(img);
+            }
         }
     }
 }
---
+++ HtmlCanvasApp/HtmlCanvasApp/Scripts/main.js
@@ -0,0 +1,129 @@
+﻿var ctx, fillStyle = '', DrawingTypes = '', canvas, rect = {}, strokeStyle = '';
+
+$(document).ready(function () {
+    canvas = document.getElementById("the-canvas");
+    ctx = canvas.getContext("2d");
+});
+
+$('body').on('keydown',
+    function (e) {
+        clear();
+        switch (e.which) {
+            case 83:
+                //Kare
+                drawSquare();
+                break;
+            case 84:
+                //Ucgen
+                drawTriangle();
+                break;
+            case 67:
+                //Daire
+                drawCircle();
+                break;
+            case 69:
+                //Elips
+                drawOval();
+                break;
+        }
+        draw();
+    });
+
+
+function clear() {
+    canvas.width = canvas.width;
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.fillStyle = fillStyle;
+    ctx.strokeStyle = strokeStyle;
+
+    switch (DrawingTypes) {
+        case "FillRect":
+            ctx.rect(rect.startX, rect.startY, rect.w, rect.h);
+            break;
+        case "FillCircle":
+            ctx.arc(rect.startX, rect.startY, rect.radiusNew, rect.w, rect.h);
+            break;
+        case "FillEllipse":
+            ctx.ellipse(rect.startX, rect.startY, rect.rx, rect.ry, 0, 0, Math.PI * 2);
+            ctx.restore();
+        case "FreeDraw":
+            break;
+    }
+    ctx.fill();
+    ctx.stroke();
+}
+
+function drawCircle() {
+    //Daire
+    DrawingTypes = "FillCircle";
+    rect = {
+        startX: 5,
+        startY: 5,
+        radiusNew: 25,
+        w: 0,
+        h: (2 * Math.PI)
+    };
+    fillStyle = 'green';
+    strokeStyle = 'orange';
+}
+
+function drawSquare() {
+    //Kare,
+    DrawingTypes = "FillRect";
+
+    rect = {
+        startX: 5,
+        startY: 5,
+        w: 50,
+        h: 50
+    };
+    fillStyle = 'white';
+    strokeStyle = 'green';
+}
+
+function drawOval() {
+    //Elips
+    DrawingTypes = "FillEllipse";
+
+    rect = {
+        startX: 100,
+        startY: 100,
+        rx: 50,
+        ry: 75
+    };
+    fillStyle = 'green';
+}
+
+
+function drawTriangle() {
+    ////Ucgen
+    DrawingTypes = "FreeDraw";
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 0);
+    ctx.lineTo(100, 200);
+    ctx.fill();
+}
+
+function saveImage() {
+
+    var imageNew = document.getElementById("the-canvas").toDataURL("image/png");
+    imageNew = imageNew.replace('data:image/png;base64,', '');
+
+    $.ajax({
+        type: 'POST',
+        url: '/home/SaveImage',
+        data: '{ "imageData" : "' + imageNew + '" }',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (msg) {
+            alert('Image saved to your root Folder !');
+        }
+    });
+
+
+}
